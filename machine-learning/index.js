babb745a730dfd2e512e67843d8782341a014342
Victor Cavalcante <victor.ra.cavalcante@gmail.com>
2018-06-03 23:42:52 -0300

    Refactoring ml-dataset algorithm;

--- machine-learning/index.js
+++ machine-learning/index.js
@@ -1,87 +1,79 @@
+'use strict';
 const KNN = require('ml-knn');
 const csv = require('csvtojson');
 const prompt = require('prompt');
-let knn;
+
 const csvFilePath = 'iris.csv'; // Data
 const names = ['sepalLength', 'sepalWidth', 'petalLength', 'petalWidth', 'type']; // For header
 
-let seperationSize; // To seperate training and test data
+let knn;
+let trainingSetX = [],
+    trainingSetY = [],
+    testSetX = [],
+    testSetY = [];
 
-let data = [], X = [], y = [];
+initialize();
 
-let trainingSetX = [], trainingSetY = [], testSetX = [], testSetY = [];
+function initialize() {
+    let dataset = [];
+    let $getData = csv({ noheader: true, headers: names })
+        .fromFile(csvFilePath)
+        .on('json', (jsonObj) => { dataset.push(jsonObj); }); // Push each object to data Array
 
-csv({noheader: true, headers: names})
-    .fromFile(csvFilePath)
-    .on('json', (jsonObj) => {
-        data.push(jsonObj); // Push each object to data Array
-    })
-    .on('done', (error) => {
-        seperationSize = 0.7 * data.length;
-        data = shuffleArray(data);
-        dressData();
+    $getData.on('done', (error) => {
+        // Prepare datasets (training_set & test_set)
+        let trainSize = 0.7 * dataset.length; // 70% of the dataset
+        divideDatasets(dataset, trainSize);
+
+        // Train & Predict
+        const dataResult = trainAndPredict();
+
+        testDataset(dataResult);
+        predict();
     });
+}
 
-function dressData() {
+function divideDatasets(dataset, trainingSetSize) {
+    let typesList, typesSet = new Set();
+    let X_DATA = [], Y_DATA = [];
 
-    /**
-     * There are three different types of Iris flowers
-     * that this dataset classifies.
-     *
-     * 1. Iris Setosa (Iris-setosa)
-     * 2. Iris Versicolor (Iris-versicolor)
-     * 3. Iris Virginica (Iris-virginica)
-     *
-     * We are going to change these classes from Strings to numbers.
-     * Such that, a value of type equal to
-     * 0 would mean setosa,
-     * 1 would mean versicolor, and
-     * 3 would mean virginica
-     */
+    // Shuffling dataset
+    dataset = shuffleArray(dataset);
 
-    let types = new Set(); // To gather UNIQUE classes
+    // Gathering unique classes & purging as an array
+    dataset.forEach(row => typesSet.add(row.type));
+    typesList = [ ...typesSet ];
 
-    data.forEach((row) => {
-        types.add(row.type);
-    });
-
-    typesArray = [...types]; // To save the different types of classes.
-
-    data.forEach((row) => {
+    // Turning string values to floats & converting headers to identifiers
+    dataset.forEach(row => {
         let rowArray, typeNumber;
 
         rowArray = Object.keys(row).map(key => parseFloat(row[key])).slice(0, 4);
-
-        typeNumber = typesArray.indexOf(row.type); // Convert type(String) to type(Number)
-
-        X.push(rowArray);
-        y.push(typeNumber);
+        typeNumber = typesList.indexOf(row.type);
+        X_DATA.push(rowArray);
+        Y_DATA.push(typeNumber);
     });
 
-    trainingSetX = X.slice(0, seperationSize);
-    trainingSetY = y.slice(0, seperationSize);
-    testSetX = X.slice(seperationSize);
-    testSetY = y.slice(seperationSize);
-
-    train();
+    trainingSetX = X_DATA.slice(0, trainingSetSize);
+    trainingSetY = Y_DATA.slice(0, trainingSetSize);
+    testSetX = X_DATA.slice(trainingSetSize);
+    testSetY = Y_DATA.slice(trainingSetSize);
 }
 
-function train() {
+function trainAndPredict() {
     knn = new KNN(trainingSetX, trainingSetY, {k: 7});
-    test();
+    return knn.predict(testSetX);
 }
 
-function test() {
-    const result = knn.predict(testSetX);
+function testDataset(result) {
     const testSetLength = testSetX.length;
     const predictionError = error(result, testSetY);
-    console.log(`Test Set Size = ${testSetLength} and number of Misclassifications = ${predictionError}`);
-    predict();
+    console.log(`>>> Test Set Size = ${testSetLength}\n>>> Number of Misclassifications = ${predictionError}`);
 }
 
 function error(predicted, expected) {
     let misclassifications = 0;
-    for (var index = 0; index < predicted.length; index++) {
+    for (let index = 0; index < predicted.length; index++) {
         if (predicted[index] !== expected[index]) {
             misclassifications++;
         }
@@ -95,23 +87,20 @@ function predict() {
 
     prompt.get(['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width'], function (err, result) {
         if (!err) {
-            for (var key in result) {
-                temp.push(parseFloat(result[key]));
+            for (let key in result) {
+                if(result.hasOwnProperty(key)){
+                    temp.push(parseFloat(result[key]));
+                }
             }
             console.log(`With ${temp} -- type =  ${knn.predict(temp)}`);
         }
     });
 }
 
-/**
- * https://stackoverflow.com/a/12646864
- * Randomize array element order in-place.
- * Using Durstenfeld shuffle algorithm.
- */
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
